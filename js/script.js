cded8d9684fa7431143439f6c2e9d0a6d6db3819
Gleb <glebslivko777@gmail.com>
2019-10-13 16:52:42 +0300

    hide-modal-display-form

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const modalButton = document.getElementById("openModalButton")
 const modal = document.getElementById("modal")
 const closeModal = document.getElementById("closeModal")
 const chooseDoctor = document.getElementById("chooseDoctorId")
+const modalContent = document.querySelector('#modalContentId')
+console.log(modalContent);
 
 modalButton.addEventListener("click", (e) => {
   if (e.target === modal) {
@@ -19,8 +21,20 @@ closeModal.addEventListener("click", (e) => {
   modal.style.display = "none"
 })
 
-chooseDoctor.addEventListener("click", (e) => {
- pacient.createWrapper()
+// chooseDoctor.addEventListener("click", (e) => {
+//  pacient.createWrapper()
+// })
+
+chooseDoctor.addEventListener("change", (event) => {
+  console.log("keks");
+  pacient.createWrapper()
+  if (event.target.value === 'Кардиолог') {
+    console.log("CORDIOLIG");
+  } else if (event.target.value === 'Терапевт<') {
+    console.log("THERAPIST");
+  } else if (event.target.value === 'Стоматолог') {
+    console.log("dantistIdT");
+  }
 })
 
 
@@ -32,6 +46,26 @@ chooseDoctor.addEventListener("click", (e) => {
 //     return false
 //   }
 // })
+const clickBtn = document.createElement('button');
+clickBtn.innerText = 'Click me';
+document.querySelector('script').before(clickBtn);
+
+
+
+// const rectangle = document.createElement("div");
+// rectangle.classList.add("rectangle");
+// rectangle.style.backgroundColor = "red";
+// rectangle.style.width = "100px";
+// rectangle.style.height = "100px";
+// document.querySelector('script').before(rectangle);
+
+// var iDiv = document.createElement('div');
+// iDiv.id = 'block';
+// iDiv.className = 'block';
+// iDiv.style.width = "100px";
+// iDiv.style.height = "100px";
+// iDiv.style.backgroundColor = "black";
+// document.getElementsByTagName('body')[0].appendChild(iDiv);
 
 
 // _____________________functional_____________
@@ -43,12 +77,30 @@ class Visit {
     this.visitPurpose = visitPurpose;
   }
   createWrapper() {
-    const container = document.createElement('div');
+    // const container = document.createElement("div");
+    // container.classList.add("container");
+    // container.style.backgroundColor = "red";
+    // container.style.width = "100px";
+    // container.style.height = "100px";
+    // document.querySelector('modal').before(container)
+
+    var container = document.createElement('div');
+    container.id = 'block';
+    container.className = 'block';
+    container.style.width = "100px";
+    container.style.height = "100px";
+    container.style.backgroundColor = "black";
+    document.getElementById('modal').appendChild(container);
+
+    modalContent.style.display = 'none'; 
+
+
+    return console.log('3 Доктора');
+
     const doctorCardiolog = document.querySelector('#cardiologId');
-    const doctorDherapist = document.querySelector('#therapistId');
+    const doctorTherapist = document.querySelector('#therapistId');
     const doctorDantist = document.querySelector('#dantistId');
-    return console.log(doctorCardiolog,doctorDherapist,doctorDantist);
-    
+
     doctorClass.addEventListener("click", (e) => {
       if (doctorClass.value == event.target.value) {
         selectedDoctor = event.targert.value

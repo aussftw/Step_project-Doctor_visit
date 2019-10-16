7d760f9557aea3b571f36c58257d7e2f7a6541ad
aussftw <shagronaus@gmail.com>
2019-10-16 17:36:56 +0300

    cleaning

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// import { FormCreator } from "../classes"
-// console.log(FormCreator)
-
 //=========================== modal ===========================//
 
 const modalButton = document.getElementById("openModalButton")
@@ -23,10 +20,11 @@ closeModal.addEventListener("click", e => {
   modal.style.display = "none"
 })
 
-// doctorChecker
+//=========================== Doctor checker ===========================//
 
 chooseDoctor.addEventListener("change", event => {
-  createdForm.render()
+  // createdForm.createForm()
+
   if (event.target.value === "Кардиолог") {
     console.log("Кардиолог")
     const cardio = new FormCreator(
@@ -42,14 +40,18 @@ chooseDoctor.addEventListener("change", event => {
   } else if (event.target.value === "Терапевт") {
     console.log("Терапевт")
     const therapist = new FormCreator("Oleg", "BrainDamage", 57)
+    therapist.render()
   } else if (event.target.value === "Стоматолог") {
     console.log("Стоматолог")
     const dentist = new FormCreator("Alma", "rotten_somewhere", "12.11.2017")
+    dentist.render()
   } else {
     return false
     console.log("check failed")
   }
 })
+
+//=========================== Form construcor ===========================//
 
 class FormCreator {
   constructor(fullName, visitPurpose, visitComment, ...formElemnts) {
@@ -62,18 +64,18 @@ class FormCreator {
   //create form to the memory
 
   createForm() {
-    console.log()
     const formInputs = document.createElement("form")
     for (let i = 0; i < this._formElements; i++) {
       const element = document.createElement(this.formElemnts[i].name)
       element.type = this._formElements[i].type
       element.placeholder = this._formElements[i].placeholder
       element.classList.add("inputData")
-      form.append(element.render())
+      form.append(element)
     }
+    return form
   }
 
-  // form builder
+  // Form render
 
   render() {
     //creating div-wrapper for form
@@ -81,33 +83,6 @@ class FormCreator {
     const container = document.createElement("div")
     container.classList.add("form-wrapper")
     document.getElementById("modal").appendChild(container)
-
-    //creating form
-
-    const form = document.createElement("form")
-    form.classList.add("form")
-    container.appendChild(form)
-
-    // creating input for FullName
-
-    const inputFullName = document.createElement("input")
-    inputFullName.classList.add("inputData")
-    inputFullName.placeholder = "Ф.И.О"
-    form.appendChild(inputFullName)
-
-    // creating input for Visit Purpose
-
-    const inputVisitPurpose = document.createElement("input")
-    inputVisitPurpose.classList.add("inputData")
-    inputVisitPurpose.placeholder = "Цель визита"
-    form.appendChild(inputVisitPurpose)
-
-    //creating text area for comments
-
-    const textAreaComment = document.createElement("textArea")
-    textAreaComment.classList.add("inputData")
-    textAreaComment.placeholder = "Вы можете оставить свой комментарий здесь"
-    form.appendChild(textAreaComment)
 
     // button "Create Visit" builder
 
@@ -117,11 +92,37 @@ class FormCreator {
     container.appendChild(createVisitButton)
 
     // hiding previous modal
+
     modalContent.style.display = "none"
 
-    const doctorCardiolog = document.querySelector("#cardiologId")
-    const doctorTherapist = document.querySelector("#therapistId")
-    const doctorDantist = document.querySelector("#dantistId")
+    createForm()
+
+    //creating form
+
+    // const form = document.createElement("form")
+    // form.classList.add("form")
+    // container.appendChild(form)
+
+    // creating input for FullName
+
+    // const inputFullName = document.createElement("input")
+    // inputFullName.classList.add("inputData")
+    // inputFullName.placeholder = "Ф.И.О"
+    // form.appendChild(inputFullName)
+
+    // creating input for Visit Purpose
+
+    // const inputVisitPurpose = document.createElement("input")
+    // inputVisitPurpose.classList.add("inputData")
+    // inputVisitPurpose.placeholder = "Цель визита"
+    // form.appendChild(inputVisitPurpose)
+
+    //creating text area for comments
+
+    // const textAreaComment = document.createElement("textArea")
+    // textAreaComment.classList.add("inputData")
+    // textAreaComment.placeholder = "Вы можете оставить свой комментарий здесь"
+    // form.appendChild(textAreaComment)
   }
 
   // createFields(mainClass, ...classes) {
@@ -137,4 +138,3 @@ class FormCreator {
 }
 
 const createdForm = new FormCreator("NAme Full", "atat", "ur comment")
-createdForm.createForm()
